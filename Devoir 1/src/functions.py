27832cb3a6b4266ffe923fbb41a302a837c1e237
E'ouann Kerino <57634751+Eouann@users.noreply.github.com>
2025-02-09 14:34:04 -0500

    Structuration code et code des erreurs L1, L2 et Linf

--- Devoir 1/src/functions.py
+++ Devoir 1/src/functions.py
@@ -28,3 +28,68 @@ C_r = 0.25*S/Deff*R**2*(r**2/R**2 - 1) + Ce
 C_analytique = sp.lambdify(r, C_r, modules=['numpy'])
 x_values = np.linspace(0, R, 500)
 y_values = C_analytique(x_values)
+
+# Transformation de la fonction analytique sympy en fonction traçable par matplotlib avec Ntot points
+C_analytique = sp.lambdify(r, C_r, modules=['numpy'])
+x_Ntotvalues = np.linspace(0, R, Ntot)
+y_Ntotvalues = C_analytique(x_values)
+
+
+#####################################################
+# Calcul des coefficients pour Ntot points CAS 1 et 2
+#####################################################
+def Coefficients(Ntot):
+    delta_r=R/(Ntot-1)
+    r_i=np.zeros(Ntot)          # Vecteur des N points r_i également espacées
+    for i in range(len(r_i)):
+        r_i[i] = delta_r*i
+
+    # Calcul des coefficients devant les Ci+1, Ci et Ci-1
+    # CAS 1
+    a1=np.zeros(Ntot)       # Coefficient devant Ci+1
+    b1=np.zeros(Ntot)       # Coefficient devant Ci
+    c1=np.zeros(Ntot)       # Coefficient devant Ci-1
+    for i in range(1,len(r_i)):
+        a1[i]=1/delta_r**2+1/(r_i[i]*delta_r)
+        b1[i]=-(2/delta_r**2+1/(r_i[i]*delta_r))
+        c1[i]=1/delta_r**2
+
+    # CAS 2
+    a2=np.zeros(Ntot)       # Coefficient devant Ci+1
+    b2=np.zeros(Ntot)       # Coefficient devant Ci
+    c2=np.zeros(Ntot)       # Coefficient devant Ci-1
+    for i in range(1,len(r_i)):
+        a2[i]=1/delta_r**2+1/(r_i[i]*2*delta_r)
+        b2[i]=-2/delta_r**2
+        c2[i]=1/delta_r**2-1/(r_i[i]*2*delta_r)
+    return a1,b1,c1,a2,b2,c2,r_i,delta_r
+
+
+###########################################
+# Calcul des cncentrations pour Ntot points
+###########################################
+def Concentrations(a,b,c):
+    # Fonction de calcul des N concentrations en différences finies
+    C_i = np.zeros(Ntot)
+    matA = np.zeros((Ntot,Ntot))
+    vectB = np.zeros(Ntot)
+
+    # Condition de Neumann à i = 0
+    matA[0,0] = 1
+    matA[0,1] = -1
+    vectB[0] = 0
+
+    # Condition de Dirichlet à i = N
+    matA[-1,-1] = 1
+    vectB[-1] = Ce
+
+    # Algorithmes differences finies
+    for i in range(1,len(C_i)-1):
+        matA[i,i-1] = c[i]
+        matA[i,i] = b[i]
+        matA[i,i+1] = a[i]
+
+        vectB[i] = S/Deff
+
+    C_i = np.linalg.solve(matA,vectB)
+    return C_i
--- Devoir 1/src/main.py
+++ Devoir 1/src/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import config
 import functions
+import errors
 
 
 ############################
@@ -18,68 +19,56 @@ R = config.R            # en m
 S = config.S            # en mol/m^3/s
 
 
-#####################################################
-# Calcul en différences finies pour Ntot points CAS 1
-#####################################################
-delta_r=R/(Ntot-1)
-r_i=np.zeros(Ntot)          # Vecteur des N points r_i également espacées
-for i in range(len(r_i)):
-    r_i[i] = delta_r*i
+# Importation des coeficients pour Ntot points CAS 1 et 2
+a1,b1,c1,a2,b2,c2,r_i,delta_r = functions.Coefficients(Ntot)
 
-# Calcul des coefficients devant les Ci+1, Ci et Ci-1
 
-# CAS 1
-a1=np.zeros(Ntot)       # Coefficient devant Ci+1
-b1=np.zeros(Ntot)       # Coefficient devant Ci
-c1=np.zeros(Ntot)       # Coefficient devant Ci-1
-for i in range(1,len(r_i)):
-    a1[i]=1/delta_r**2+1/(r_i[i]*delta_r)
-    b1[i]=-(2/delta_r**2+1/(r_i[i]*delta_r))
-    c1[i]=1/delta_r**2
-
-# CAS 2
-a2=np.zeros(Ntot)       # Coefficient devant Ci+1
-b2=np.zeros(Ntot)       # Coefficient devant Ci
-c2=np.zeros(Ntot)       # Coefficient devant Ci-1
-for i in range(1,len(r_i)):
-    a2[i]=1/delta_r**2+1/(r_i[i]*2*delta_r)
-    b2[i]=-2/delta_r**2
-    c2[i]=1/delta_r**2-1/(r_i[i]*2*delta_r)
-
-def Concentrations(a,b,c):
-    # Fonction de calcul des N concentrations en différences finies
-    C_i = np.zeros(Ntot)
-    matA = np.zeros((Ntot,Ntot))
-    vectB = np.zeros(Ntot)
-
-    # Condition de Neumann à i = 0
-    matA[0,0] = 1
-    matA[0,1] = -1
-    vectB[0] = 0
-
-    # Condition de Dirichlet à i = N
-    matA[-1,-1] = 1
-    vectB[-1] = Ce
-
-    # Algorithmes differences finies
-    for i in range(1,len(C_i)-1):
-        matA[i,i-1] = c[i]
-        matA[i,i] = b[i]
-        matA[i,i+1] = a[i]
-
-        vectB[i] = S/Deff
-
-    C_i = np.linalg.solve(matA,vectB)
-    return C_i
-
-Concentrations_CAS1=Concentrations(a1,b1,c1)
-Concentrations_CAS2=Concentrations(a2,b2,c2)
+# Calcul des concentrations pour les deux cas et pour le cas analytique
+Concentrations_CAS1=functions.Concentrations(a1,b1,c1)
+Concentrations_CAS2=functions.Concentrations(a2,b2,c2)
 x_values = functions.x_values
 y_values = functions.y_values
 
+
 # Tracé des fonctions analytiques et différences finies
+
 plt.figure()
-plt.plot(x_values, y_values)
-plt.plot(r_i, Concentrations_CAS1, 'o', color='red')
-plt.plot(r_i, Concentrations_CAS2, 'o', color='green')
+plt.plot(x_values, y_values, label='Analytique')
+plt.plot(r_i, Concentrations_CAS1, 'o', color='red', label='Cas 1 - numérique')
+plt.plot(r_i, Concentrations_CAS2, 'o', color='green',label='Cas 2 - numérique')
+plt.xlabel('r (m)')
+plt.ylabel('Concentration (mol/m^3)')
+plt.grid()
+plt.legend()
+plt.show()
+
+# Tracé des erreurs L1, L2 et Linf CAS1
+vectDelta_r = []
+vectL1 = []
+vectL2 = []
+vectLinf = []
+
+for N in range(10, 1010, 100):
+    a1,b1,c1,a2,b2,c2,r_i,delta_r = functions.Coefficients(N)
+    vectDelta_r.append(delta_r)
+    Concentrations_CAS1=functions.Concentrations(a1,b1,c1)
+    L1=errors.ErreurL1(Concentrations_CAS1,functions.x_Ntotvalues)
+    vectL1.append(L1)
+    L2=errors.ErreurL2(Concentrations_CAS1,functions.x_Ntotvalues)
+    vectL2.append(L2)
+    Linf=errors.ErreurLinf(Concentrations_CAS1,functions.x_Ntotvalues)
+    vectLinf.append(Linf)
+
+
+
+plt.figure()
+plt.plot(vectDelta_r, vectL1, 'o', color='red', label='L1')
+plt.plot(vectDelta_r, vectL2, 'o', color='green', label='L2')
+plt.plot(vectDelta_r, vectLinf, 'o', color='blue', label='Linf')
+plt.legend()
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Delta r (m)')
+plt.ylabel('Erreur (mol/m^3)')
+plt.grid()
 plt.show()
