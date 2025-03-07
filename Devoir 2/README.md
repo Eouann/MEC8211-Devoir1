b7a92ea21640c3e49957f59c814d7da085ca3d71
E'ouann Kerino <57634751+Eouann@users.noreply.github.com>
2025-03-07 15:16:58 -0500

    Ajout de la convergence temporelle
    Problème, les codes des convergence retourne des ordres de convergence à 0

--- Devoir 2/README.md
+++ Devoir 2/README.md
@@ -8,4 +8,4 @@ Explication des fichiers python dans /src :
 - /src/main.py pour exécuter le code.
 - /src/config.py pour la définition des constantes.
 - /src/errors.py pour le code des erreurs L1, L2, Linf.
-- /src/functions.py pour le code des fonctions : analytique et de calcul des concentrations en différences finies avec N points.
+- /src/functions.py pour le code des fonctions : terme source et de calcul des concentrations en différences finies avec N points spatiaux et N temporels.
--- Devoir 2/src/main.py
+++ Devoir 2/src/main.py
@@ -84,7 +84,7 @@ list_delta_r=[0.25, 0.166666667, 0.125, 0.055555556,
               0.026315789, 0.017241379, 0.010204082, 
               0.005050505, 0.002512563, 0.001672241, 
               0.001002004, 0.000500501]
-delta_t = 3.1536e7      # 1 an en s
+delta_t = (3.1536e7)      # 1 an en s
 vectDelta_r = np.zeros(len(list_delta_r))
 vectL1 = np.zeros(len(list_delta_r))
 vectL2 = np.zeros(len(list_delta_r))
@@ -134,4 +134,62 @@ plt.xlabel('Delta r (m)')
 plt.ylabel('Erreur (mol/m^3)')
 plt.grid(True, which="both", ls="--")
 plt.savefig('Devoir 2/results/erreurs_spatiales.png')
+plt.show()
+
+
+##############################################################################
+# Analyse de convergence temporelle avec delta spatial fixé à 0,125 m (5 points)
+##############################################################################
+list_delta_t=[935000000, 415555555.55555600, 128965517.24137900, 76326530.61224490,
+37777777.77777780, 12508361.20401340, 7494989.97995992, 3743743.74374374]
+delta_r = 0.125      # en m
+vectDelta_t = np.zeros(len(list_delta_t))
+vectL1 = np.zeros(len(list_delta_t))
+vectL2 = np.zeros(len(list_delta_t))
+vectLinf = np.zeros(len(list_delta_t))
+
+for i in (range(len(list_delta_t))):
+    delta_t = list_delta_t[i]
+    N = Tf / delta_t + 1
+    N = int(N)
+    
+    C_i, r_i, t_i=functions.Concentrations(delta_r, delta_t)
+    y_values = C_chapeau(0.25, t_i)
+    vectDelta_t[i]=delta_t
+
+    L1=errors.ErreurL1(C_i[2],y_values,N)
+    vectL1[i]=L1
+    L2=errors.ErreurL2(C_i[2],y_values,N)
+    vectL2[i]=L2
+    Linf=errors.ErreurLinf(C_i[2],y_values)
+    vectLinf[i]=Linf
+
+plt.figure()
+
+# L1
+plt.plot(vectDelta_t, vectL1, 'o', color='red', label='L1')
+slope_L1, intercept_L1, r_value_L1, p_value_L1, std_err_L1 = linregress(np.log(vectDelta_t[4:]), np.log(vectL1[4:]))
+y_pred_L1 =  np.exp(intercept_L1) * vectDelta_t[4:]**slope_L1
+plt.plot(vectDelta_t[4:], y_pred_L1, '--', color='red', label=f'Ordre de convergence L1: {slope_L1}')
+
+# L2
+plt.plot(vectDelta_t, vectL2, 'o', color='green', label='L2')
+slope_L2, intercept_L2, r_value_L2, p_value_L2, std_err_L2 = linregress(np.log(vectDelta_t[4:]), np.log(vectL2[4:]))
+y_pred_L2 = np.exp(intercept_L2) * vectDelta_t[4:]**slope_L2
+plt.plot(vectDelta_t[4:], y_pred_L2, '--', color='green', label=f'Ordre de convergence L2: {slope_L2}')
+
+# Linf
+plt.plot(vectDelta_t, vectLinf, 'o', color='blue', label='Linf')
+slope_L3, intercept_L3, r_value_L3, p_value_L3, std_err_L3 = linregress(np.log(vectDelta_t[4:]), np.log(vectLinf[4:]))
+y_pred_Linf = np.exp(intercept_L3) * vectDelta_t[4:]**slope_L3
+plt.plot(vectDelta_t[4:], y_pred_Linf, '--', color='blue', label=f'Ordre de convergence Linf: {slope_L3}')
+
+plt.title('Erreurs L1, L2 et Linf en fonction du nombre de points N temporels')
+plt.legend()
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Delta t (s)')
+plt.ylabel('Erreur (mol/m^3)')
+plt.grid(True, which="both", ls="--")
+plt.savefig('Devoir 2/results/erreurs_temporels.png')
 plt.show()
